a93d382fb7f5086e0b2f51b6b506e2acadf010e4
Mark Longair <mhl@pobox.com>
2021-12-07 20:04:54 +0000

    Switch to using min() rather than a for loop

--- 2021/07/part1.py
+++ 2021/07/part1.py
@@ -3,18 +3,14 @@
 with open("input-jenny.txt") as f:
     positions = [int(p) for p in f.read().split(",")]
 
-min_position = min(positions)
-max_position = max(positions)
-
 
 def fuel_to_move_to_position(aligning_position, starting_positions):
     return sum(abs(p - aligning_position) for p in starting_positions)
 
 
-best_fuel_cost_so_far = None
-for p in range(min_position, max_position + 1):
-    fuel_cost = fuel_to_move_to_position(p, positions)
-    if best_fuel_cost_so_far is None or fuel_cost < best_fuel_cost_so_far:
-        best_fuel_cost_so_far = fuel_cost
+best_fuel_cost = min(
+    fuel_to_move_to_position(p, positions)
+    for p in range(min(positions), max(positions) + 1)
+)
 
-print(best_fuel_cost_so_far)
+print(best_fuel_cost)
--- 2021/07/part2.py
+++ 2021/07/part2.py
@@ -3,9 +3,6 @@
 with open("input-jenny.txt") as f:
     positions = [int(p) for p in f.read().split(",")]
 
-min_position = min(positions)
-max_position = max(positions)
-
 
 def triangular_number(n):
     return (n * (n + 1)) // 2
@@ -17,10 +14,9 @@ def fuel_to_move_to_position(aligning_position, starting_positions):
     )
 
 
-best_fuel_cost_so_far = None
-for p in range(min_position, max_position + 1):
-    fuel_cost = fuel_to_move_to_position(p, positions)
-    if best_fuel_cost_so_far is None or fuel_cost < best_fuel_cost_so_far:
-        best_fuel_cost_so_far = fuel_cost
+best_fuel_cost = min(
+    fuel_to_move_to_position(p, positions)
+    for p in range(min(positions), max(positions) + 1)
+)
 
-print(best_fuel_cost_so_far)
+print(best_fuel_cost)
