d49ddd34e69fb23e9354ded45134b48b778c1884
Mark Longair <mhl@pobox.com>
2021-12-08 23:13:33 +0000

    Solutions for the 8th of December
    I'm not proud of brute-forcing part 2
    
    Co-Authored-By: Jenny Duckett <git@jennyduckett.com>

--- 2021/08/part1.py
+++ 2021/08/part1.py
@@ -3,17 +3,12 @@
 import re
 from collections import namedtuple
 
-with open("input-jenny.txt") as f:
-    raw_entries = f.read()
-
-matches = re.findall("^([a-g]+) ([a-g]+) ([a-g]+) ([a-g]+) ([a-g]+) ([a-g]+) ([a-g]+) ([a-g]+) ([a-g]+) ([a-g]+) \| ([a-g]+) ([a-g]+) ([a-g]+) ([a-g]+$)", raw_entries, flags=re.MULTILINE)
-
-Entry = namedtuple("Entry", ["signal_patterns", "output_value"])
-
-entries = tuple(Entry(signal_patterns=match[:10], output_value=match[10:]) for match in matches)
+with open("input.txt") as f:
+    entries = [re.findall("[a-g]+", line) for line in f]
 
 unique_number_lengths = (2, 3, 4, 7)
-
-unique_number_lengths_count = sum([1 for e in entries for v in e.output_value if len(v) in unique_number_lengths])
+unique_number_lengths_count = sum(
+    [1 for e in entries for v in e[-4:] if len(v) in unique_number_lengths]
+)
 
 print(unique_number_lengths_count)
---
+++ 2021/08/part2.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+from itertools import permutations
+import re
+from collections import namedtuple
+
+with open("input.txt") as f:
+    entries = [re.findall("[a-g]+", line) for line in f]
+
+SEGMENTS_CORRECT = {
+    "abcefg": "0",
+    "cf": "1",
+    "acdeg": "2",
+    "acdfg": "3",
+    "bcdf": "4",
+    "abdfg": "5",
+    "abdefg": "6",
+    "acf": "7",
+    "abcdefg": "8",
+    "abcdfg": "9",
+}
+
+# Order the segment strings by length from shortest to longest, since
+# checking the two letter string represting "1" on the display first
+# is the fastest way to cut down the search space.
+SEGMENTS_CORRECT_ORDERED = sorted(SEGMENTS_CORRECT.keys(), key=lambda s: len(s))
+
+# This is a generator function so that we don't have to map any more
+# letters once we've established this can't be the right mapping
+def displays_from_mapping(mapping_correct_to_wrong):
+    for correct_display in SEGMENTS_CORRECT_ORDERED:
+        wrong_letters = [mapping_correct_to_wrong[l] for l in correct_display]
+        yield "".join(sorted(wrong_letters))
+
+def brute_force_entry(entry):
+    # Sort each of the strings alphabetically to normalize them for lookup
+    displays = ("".join(sorted(d)) for d in entry[:10])
+    displays_set = set(displays)
+    for letters_permutation in permutations('abcdefg'):
+        mapping_correct_to_wrong = {
+            correct: wrong for correct, wrong in zip('abcdefg', letters_permutation)
+        }
+        correct_mapping = True
+        for wrong_display in displays_from_mapping(mapping_correct_to_wrong):
+            if wrong_display not in displays_set:
+                correct_mapping = False
+                break
+        if correct_mapping:
+            break
+    # Now we've got the mapping, use it to translate the 4 output displays
+    # into the correct 4 digit number
+    mapping_wrong_to_correct = {v: k for k, v in mapping_correct_to_wrong.items()}
+    correct_output_numbers = [
+        SEGMENTS_CORRECT["".join(sorted([mapping_wrong_to_correct[l] for l in output_wrong]))]
+        for output_wrong in entry[10:]
+    ]
+    return int("".join(correct_output_numbers))
+
+print(sum(brute_force_entry(entry) for entry in entries))
