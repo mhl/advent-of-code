e142df2c8bab7aa00bb0ba8e117aeea4f0798768
Jenny Duckett <git@jennyduckett.com>
2021-12-15 22:00:42 +0000

    Part 2 for day 15
    We tried using heapq in our first attempt at part 1, but gave up on it when we
    realised that items would appear multiple times in the heap as it wasn't
    possible to update existing items in it. We gave up on it too quickly to
    realise that we could use the distance in the item to check if it was a
    current entry by comparing it to the distances we're tracking in dist - this
    blog post helped us with that:
    
    https://bradfieldcs.com/algos/graphs/dijkstras-algorithm/
    
    Co-Authored-By: Mark Longair <mhl@pobox.com>

---
+++ 2021/15/part2.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+from heapq import heappush, heappop
+
+with open("input-jenny.txt") as f:
+    original_grid = [[int(digit) for digit in line.rstrip()] for line in f]
+
+def generate_giant_grid(original_grid):
+    width = len(original_grid[0])
+    height = len(original_grid)
+    giant_grid = [[None]*width*5 for _ in range(height*5)]
+    for i in range(5):
+        for j in range(5):
+            increase = i + j
+            for y in range(height):
+                for x in range(width):
+                    original_value = original_grid[y][x]
+                    new_value = original_value + increase
+                    if new_value > 9:
+                        new_value -= 9
+                    giant_grid[y + j * height][x + i * width] = new_value
+    return giant_grid
+
+grid = generate_giant_grid(original_grid)
+
+width = len(grid[0])
+height = len(grid)
+start = (0,0)
+target = (width - 1, height - 1)
+
+large_sentinel = 1000000
+
+def get_adjacent_coords(x, y):
+    return tuple(
+        (i, j)
+        for i, j in (
+            (x, y - 1),
+            (x - 1, y),
+            (x + 1, y),
+            (x, y + 1),
+        )
+        if 0 <= i < width and 0 <= j < height
+    )
+
+dist = {}
+prev = {}
+queue = []
+
+for y in range(height):
+    for x in range(width):
+        coord = (x, y)
+        if coord == start:
+            tentative_distance = 0
+        else:
+            tentative_distance = large_sentinel
+        dist[coord] = tentative_distance
+        prev[coord] = None
+        heappush(queue, (tentative_distance, coord))
+
+while queue:
+    u_tentative_distance, u = heappop(queue)
+
+    if u == target:
+        break
+
+    if u_tentative_distance > dist[u]:
+        continue
+
+    for adjacent_coord in get_adjacent_coords(*u):
+        x, y = adjacent_coord
+        alternative_distance = u_tentative_distance + grid[y][x]
+        if alternative_distance < dist[adjacent_coord]:
+            dist[adjacent_coord] = alternative_distance
+            prev[adjacent_coord] = u
+            heappush(queue, (alternative_distance, adjacent_coord))
+
+path_coord = target
+path_reversed = [path_coord]
+
+while path_coord != start:
+    path_coord = prev[path_coord]
+    if path_coord != start:
+        path_reversed.append(path_coord)
+
+print(sum(grid[y][x] for x, y in reversed(path_reversed)))
