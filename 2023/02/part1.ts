989440b3b27264722a4040fe0205dcd60e583d6f
Jenny Duckett <git@jennyduckett.com>
2023-12-03 22:10:18 +0000

    Reformatting with prettier

--- 2023/02/part1.ts
+++ 2023/02/part1.ts
@@ -2,29 +2,29 @@ import { promises as fs } from "fs";
 
 const VALID_BALL_COLOURS = ["red", "green", "blue"] as const;
 
-type BallColour = typeof VALID_BALL_COLOURS[number];
+type BallColour = (typeof VALID_BALL_COLOURS)[number];
 
 function isValidBallColour(s: string): s is BallColour {
     return VALID_BALL_COLOURS.includes(s as BallColour);
 }
 
 const max_per_colour: Record<BallColour, number> = {
-    "red": 12,
-    "green": 13,
-    "blue": 14
-}
+    red: 12,
+    green: 13,
+    blue: 14,
+};
 
-function check_colour (colour_number: string) {
-    const n = colour_number.split(' ')[0];
-    const c = colour_number.split(' ')[1];
+function check_colour(colour_number: string) {
+    const n = colour_number.split(" ")[0];
+    const c = colour_number.split(" ")[1];
     if (!isValidBallColour(c)) {
         throw new Error(`Invalid ball colour ${c}`);
     }
     return max_per_colour[c] >= Number(n);
 }
 
-function check_valid_subset (subset: string) {
-    const colours = subset.split(', ');
+function check_valid_subset(subset: string) {
+    const colours = subset.split(", ");
     return colours.every(check_colour);
 }
 
@@ -38,7 +38,7 @@ async function main() {
             continue;
         }
         const game_id = match[1];
-        const subsets = match[2].split('; ');
+        const subsets = match[2].split("; ");
         if (subsets.every(check_valid_subset)) {
             total += Number(game_id);
         }
