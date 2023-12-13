f6c48c347ef572c7b6879685e5c4d7eaf11affa6
Jenny Duckett <git@jennyduckett.com>
2023-12-13 23:03:16 +0000

    Day 12 WiP

--- 2023/12/part1.ts
+++ 2023/12/part1.ts
@@ -5,8 +5,24 @@ export function countSprings(s: string) {
     return matches.map((m) => m[0].length);
 }
 
+export function getConstraints(s: string) {
+    const matches = Array.from(s.matchAll(/\?/g));
+    const unknowns = matches.length;
+    const unknown_indices = matches.map(m => m.index);
+
+    const known_springs = Array.from(s.matchAll(/#/g)).length;
+
+    const counts = s.split(" ")[1].split(",").map(Number);
+    const total_springs = counts.reduce(
+        (total_so_far, c) => total_so_far + c,
+        0,
+    );
+    const springs_in_unknowns = total_springs - known_springs;
+    return {total_springs, unknowns, springs_in_unknowns, unknown_indices};
+}
+
 async function main() {
-    let lines = await getInputLines(2023, 11);
+    let lines = await getInputLines(2023, 12);
     // let lines = await getExampleLines();
     lines = lines.filter((l) => l.length > 0);
 }
--- 2023/12/test.ts
+++ 2023/12/test.ts
@@ -1,7 +1,18 @@
-import { countSprings } from "./part1";
+import { countSprings, getConstraints } from "./part1";
 
 describe("countSprings", () => {
     test("a simple example from the problem statement", () => {
         expect(countSprings("#.#.###")).toEqual([1, 1, 3]);
+    });
+});
+
+describe("getConstraints", () => {
+    test("a simple example from the problem statement", () => {
+        expect(getConstraints("???.### 1,1,3")).toEqual({
+            total_springs: 5,
+            unknowns: 3,
+            springs_in_unknowns: 2,
+            unknown_indices: [0, 1, 2]
+        });
     });
 });
